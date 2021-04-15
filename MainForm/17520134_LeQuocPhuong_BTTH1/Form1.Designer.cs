521a5b2b5b5884c643c875f6ee47e1dc608b002a
Fuwn <17520134@gm.uit.edu.vn>
2021-04-15 08:24:56 +0700

    final

--- MainForm/17520134_LeQuocPhuong_BTTH1/Form1.Designer.cs
+++ MainForm/17520134_LeQuocPhuong_BTTH1/Form1.Designer.cs
@@ -860,7 +860,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1711, 863);
             this.Controls.Add(this.tctrMovie);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
